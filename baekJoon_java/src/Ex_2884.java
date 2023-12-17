a8d812c2157764583d9a1e5806ec4eba37f12bde
Ino <sylee74133@gmail.com>
2023-12-17 21:51:34 +0900

    조건문 : 오븐 시계

--- baekJoon_java/src/Ex_2884.java
+++ baekJoon_java/src/Ex_2884.java
@@ -3,21 +3,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Ex_2884 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
-        int C = br.read();
+        int C = Integer.parseInt(br.readLine());
 
-        if ( (B + C) >= 60 ) {
-
-        }
+        B = B + C;
+        A = A + B / 60;
+        B = B % 60;
+        A = A % 24;
 
         System.out.println( A + " " + B );
-        System.out.println(B);
-        System.out.println(C);
     }
 }
