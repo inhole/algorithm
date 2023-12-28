7dae16c0c961711d0c730e1a024e5d6c0d251ff4
Ino <sylee74133@gmail.com>
2023-12-28 22:34:02 +0900

    Commit.

---
+++ baekJoon_java/src/ex/Ex_2439.java
@@ -0,0 +1,33 @@
+package ex;
+
+import java.io.IOException;
+
+/**
+ * 문제 2439번 : 별 찍기 - 2
+ *
+ * @since 2023-12-26
+ */
+public class Ex_2439 {
+    public static void main(String[] args) throws IOException {
+        /*
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine()); // 첫 줄, 테스트케이스의 개수 T
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            sb.append(" ");
+        }
+
+        for (int i = 0; i < T; i++) {
+            sb.replace(T-i-1, T-i, "*");
+            bw.write(sb.toString() + "\n");
+        }
+
+        bw.flush(); // 남아있는 데이터를 모두 출력.
+        bw.close(); // 스트림을 닫음 / TODO : 스트림은 '데이터의 흐름' 입니다.
+        */
+    }
+}
---
+++ baekJoon_java/src/ex/Ex_25314.java
@@ -0,0 +1,27 @@
+package ex;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * 문제 25314번 : 코딩은 체육과목 입니다
+ *
+ * @since 2023-12-25
+ */
+public class Ex_25314 {
+    public static void main(String[] args) throws IOException {
+        /*
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        int nByte = n / 4;
+
+        for (int i = 0; i < nByte; i++) {
+            sb.append("long ");
+        }
+        sb.append("int");
+        System.out.println(sb);
+        */
+    }
+}
